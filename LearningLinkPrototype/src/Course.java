393d3741972b6d87a48c1fd42ecd69062defbdb5
Eli Hanna <ehanna2@asu.edu>
2017-02-25 19:19:58 -0800

    Updated notes and added updated getters/setters

--- LearningLinkPrototype/src/Course.java
+++ LearningLinkPrototype/src/Course.java
@@ -2,10 +2,10 @@
 public class Course {
 	private String courseID;
 	private String courseName;
-	private String courseHomePage;
+	private String courseHomePage; //This is a URL to the courses homepage
 	private String courseOverview;
 	private Prerequisite prereq;
-	//private Teacher instructor;
+	private Teacher instructor;
 	private String whoClassFor;
 	private String briefSyllabus;
 	private String courseDifficulty;
@@ -18,62 +18,97 @@ public class Course {
 	public Course(){
 		
 	}
-	
+
 	//GETTERS AND SETTERS
-	public String getCourseID(){
+
+	public String getCourseID() {
 		return courseID;
 	}
-	public String getCourseName() {
-		return courseName;
-	}
-	public void setCourseName(String courseName) {
-		this.courseName = courseName;
-	}
-	public String getCourseHomePage() {
-		return courseHomePage;
-	}
-	public void setCourseHomePage(String courseHomePage) {
-		this.courseHomePage = courseHomePage;
-	}
-	public String getCourseOverview() {
-		return courseOverview;
-	}
-	public void setCourseOverview(String courseOverview) {
-		this.courseOverview = courseOverview;
-	}
-	public String getWhoClassFor() {
-		return whoClassFor;
-	}
-	public void setWhoClassFor(String whoClassFor) {
-		this.whoClassFor = whoClassFor;
-	}
-	public String getBriefSyllabus() {
-		return briefSyllabus;
-	}
-	public void setBriefSyllabus(String briefSyllabus) {
-		this.briefSyllabus = briefSyllabus;
-	}
-	public String getCourseDifficulty() {
-		return courseDifficulty;
-	}
-	public void setCourseDifficulty(String courseDifficulty) {
-		this.courseDifficulty = courseDifficulty;
-	}
-	public int getCourseTotalHours() {
-		return courseTotalHours;
-	}
-	public void setCourseTotalHours(int courseTotalHours) {
-		this.courseTotalHours = courseTotalHours;
-	}
-	public double getUserRating() {
-		return userRating;
-	}
-	public void setUserRating(double userRating) {
-		this.userRating = userRating;
-	}
+
 	public void setCourseID(String courseID) {
 		this.courseID = courseID;
 	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public String getCourseHomePage() {
+		return courseHomePage;
+	}
+
+	public void setCourseHomePage(String courseHomePage) {
+		this.courseHomePage = courseHomePage;
+	}
+
+	public String getCourseOverview() {
+		return courseOverview;
+	}
+
+	public void setCourseOverview(String courseOverview) {
+		this.courseOverview = courseOverview;
+	}
+
+	public Prerequisite getPrereq() {
+		return prereq;
+	}
+
+	public void setPrereq(Prerequisite prereq) {
+		this.prereq = prereq;
+	}
+
+	public Teacher getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Teacher instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getWhoClassFor() {
+		return whoClassFor;
+	}
+
+	public void setWhoClassFor(String whoClassFor) {
+		this.whoClassFor = whoClassFor;
+	}
+
+	public String getBriefSyllabus() {
+		return briefSyllabus;
+	}
+
+	public void setBriefSyllabus(String briefSyllabus) {
+		this.briefSyllabus = briefSyllabus;
+	}
+
+	public String getCourseDifficulty() {
+		return courseDifficulty;
+	}
+
+	public void setCourseDifficulty(String courseDifficulty) {
+		this.courseDifficulty = courseDifficulty;
+	}
+
+	public int getCourseTotalHours() {
+		return courseTotalHours;
+	}
+
+	public void setCourseTotalHours(int courseTotalHours) {
+		this.courseTotalHours = courseTotalHours;
+	}
+
+	public double getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(double userRating) {
+		this.userRating = userRating;
+	}
+	
 	
 	
 	
