8f89318969ea0696aa74ba9f2b622bd121bb5251
Eli Hanna <ehanna2@asu.edu>
2017-02-25 18:36:13 -0800

    Added Teacher class (child of User)

---
+++ LearningLinkPrototype/src/Teacher.java
@@ -0,0 +1,30 @@
+
+public class Teacher extends User{
+
+	private Course[] coursesTeaching;
+	private String bio;
+	
+	public Teacher() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Course[] getCoursesTeaching() {
+		return coursesTeaching;
+	}
+
+	public void setCoursesTeaching(Course[] coursesTeaching) {
+		this.coursesTeaching = coursesTeaching;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	
+	
+	
+
+}
