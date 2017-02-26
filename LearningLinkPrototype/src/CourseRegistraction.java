1d28c61aa00f4fd0773d2ad9c8e65a56b0c512f7
Eli Hanna <ehanna2@asu.edu>
2017-02-25 18:57:10 -0800

    Added CourseRegistration class

---
+++ LearningLinkPrototype/src/CourseRegistraction.java
@@ -0,0 +1,17 @@
+
+public class CourseRegistraction{
+	
+	private Student registeringStudent;
+	private Course courseToTake;
+	
+
+	public CourseRegistraction() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void onClick(){
+		//What goes here
+	}
+
+}
