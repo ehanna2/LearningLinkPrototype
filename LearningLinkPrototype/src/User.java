077cecb8a897a14748a71418b9555b42f1ca6e2f
Eli Hanna <ehanna2@asu.edu>
2017-02-25 18:26:35 -0800

    User methods added

--- LearningLinkPrototype/src/User.java
+++ LearningLinkPrototype/src/User.java
@@ -1,8 +1,66 @@
 
 public class User {
 
+	private String userID;
+	private String password;
+	private boolean loginStatus;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	protected String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isLoginStatus() {
+		return loginStatus;
+	}
+
+	public void setLoginStatus(boolean loginStatus) {
+		this.loginStatus = loginStatus;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
 
 }
