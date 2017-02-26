8ae594a338f6261197a8a4b3a8a78f67278ab32a
Eli Hanna <ehanna2@asu.edu>
2017-02-25 18:48:50 -0800

    Added CompletedCourse class (Child of Course)

---
+++ LearningLinkPrototype/src/CompletedCourse.java
@@ -0,0 +1,15 @@
+
+public class CompletedCourse extends Course {
+
+	private double grade;
+	private boolean completed;
+	
+	public CompletedCourse() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getGrade(){
+		return grade;
+	}
+
+}
